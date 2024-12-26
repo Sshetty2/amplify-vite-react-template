19764ca481ac2c739b023e2f39e7aad68bda24b6
Sachit Shetty <sachit.shetty@gmail.com>
2024-12-25 21:08:08 -0500

    remove import plugin and refactor eslint config

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
-import App from './App.tsx';
-
-import outputs from '../amplify_outputs.json';
-
+import App from './App';
+import outputs from '../amplify_outputs.json' assert { type: 'json' };
 import './index.css';
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
